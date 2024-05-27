bbd939ab88dd64b1e844234e94ed39ef537c8ee0
Rajat.bans <lazyghost@BMMDVRAJATBANS.local>
2024-05-27 20:53:07 +0530

    Updated the code :

--- langchain/history_aware/rag.py
+++ langchain/history_aware/rag.py
@@ -21,7 +21,7 @@ import gradio as gr
 DB_FAISS_PATH = "../vectorstore/db_faiss"
 CHUNK_SIZE = 512
 CHUNK_OVERLAP = 256
-SIMILARITY_THRESHOLD = 0.5
+SIMILARITY_THRESHOLD = 0.25
 embedding_model = "text-embedding-3-small"
 model_name = "gpt-3.5-turbo"
 
@@ -69,16 +69,16 @@ def setupDbAndChain(data_path):
         db = FAISS.load_local(DB_FAISS_PATH, embeddings, allow_dangerous_deserialization=True)
     
     history_aware_retriever = create_history_aware_retriever(
-        ChatOpenAI(model=model_name), db.as_retriever(), ChatPromptTemplate.from_messages(
+        ChatOpenAI(model=model_name, temperature=0), db.as_retriever(), ChatPromptTemplate.from_messages(
         [
-            ("system", "Given a chat history and the latest user question which might reference context in the chat history, formulate a standalone question which can be understood without the chat history. Do NOT answer the question, just reformulate it if needed and otherwise return it as is."),
+            ("system", """Given a chat history and the latest user question, which may reference context from the chat history, you must formulate a standalone question that can be understood without the chat history. You are strictly forbidden from using any outside knowledge. Do not, under any circumstances, answer the question. Reformulate it if necessary; otherwise, return it as is."""),
             MessagesPlaceholder("chat_history"),
             ("human", "{input}"),
         ]
     ))
-    question_answer_chain = create_stuff_documents_chain(ChatOpenAI(model=model_name), ChatPromptTemplate.from_messages(
+    question_answer_chain = create_stuff_documents_chain(ChatOpenAI(model=model_name, temperature=0), ChatPromptTemplate.from_messages(
         [
-            ("system", "You are an assistant for question-answering tasks. Use the provided context to answer the questions. If the question isn't related to the context, respond that you don't know the answer. Answer based only on the provided context, not from outside knowledge. The context is as follows: {context}"),
+            ("system", """As an assistant for question-answering tasks, your approach must be systematic and meticulous. First, identify CLUES such as keywords, phrases, contextual information, semantic relations, tones, and references that aid in determining the context of the input. Second, construct a concise diagnostic REASONING process (limiting to 130 words) based on premises supporting the INPUT relevance within the provided context. Third, utilizing the identified clues, reasoning, and input, furnish the pertinent answer for the question. Remember, you are required to use ONLY the provided context to answer the questions. If the question does not align with the context or if the context is absent, indicate that you don't know the answer. External knowledge is strictly prohibited. Failure to adhere will lead to incorrect answers. The context is as follows: {context}."""),
             MessagesPlaceholder("chat_history"),
             ("human", "{input}"),
         ]
@@ -91,9 +91,19 @@ load_dotenv(override=True)
 # chain = setupDbAndChain('../../documents/')
 chain, relevant_content = setupDbAndChain('../../131_webmd_vogon_sample1000_urlsContent_cleaned.tsv')
 chat_history = []
+curr_question_no = 1
 def echo(question, history):
+    global curr_question_no, chat_history
     response = chain.invoke({"input": question, "chat_history": chat_history})
+    if len(response['context']) == 0 or "don't know" in response['answer'] or "cannot provide an answer" in response['answer'] or "I'm sorry" in response['answer']:
+        print(f"[ INVALID QUESTION ] {question} ----------> ", response['answer'])
+        return f"Answer: Question isn't relevant to provided context."
+    print(f"[ {curr_question_no} ] {question} ----------> ", response['answer'])
+    curr_question_no += 1
     chat_history.extend([HumanMessage(content=question), response["answer"]])
+    # if len(chat_history) > 3:
+    #     chat_history = chat_history[1:]
+    # print(chat_history)
     docs_info = "\n\n".join([f"Title: {doc.metadata['title']}\nUrl: {doc.metadata['url']}\nContent: {doc.page_content}" for doc in response["context"]])
     full_response = f"Answer: {response['answer']}\n\nRetrieved Documents:\n{docs_info}"
     return full_response
