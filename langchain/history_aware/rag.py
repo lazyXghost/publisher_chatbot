18880ba71e24a67e556dc66a5b85233cb0c6ce01
Rajat.bans <lazyghost@BMMDVRAJATBANS.local>
2024-05-28 12:32:52 +0530

    Updated the code to give prompt input too

--- langchain/history_aware/rag.py
+++ langchain/history_aware/rag.py
@@ -4,8 +4,13 @@
 from dotenv import load_dotenv
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
+
 from langchain_openai import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
+
+# from langchain_community.llms import llamacpp
+# from langchain.embeddings import LlamaCppEmbeddings
+# from langchain_community.embeddings import HuggingFaceEmbeddings
 import os
 import pandas as pd
 
@@ -24,40 +29,32 @@ CHUNK_OVERLAP = 256
 SIMILARITY_THRESHOLD = 0.25
 embedding_model = "text-embedding-3-small"
 model_name = "gpt-3.5-turbo"
+# data_file_path = "../../131_webmd_vogon_sample1000_urlsContent_cleaned.tsv"
+data_file_path = "../../132_webmd_vogon_urlsContent_cleaned.tsv"
+# llama_model_path = "../../models/llama-2-7b-chat.Q4_K_M.gguf"
 
 
-def setupDbAndChain(data_path):
+def setupDbAndChainRetreiver(data_path):
     embeddings = OpenAIEmbeddings(model=embedding_model)
-    df = pd.read_csv(data_path, sep='\t')
-    # relevant_content = df['url'].apply(lambda x: ' '.join(x.split('/')[3:])).values
-    relevant_content = df['url'].values
+    # embeddings = LlamaCppEmbeddings(model_path=llama_model_path)
+    # embeddings = HuggingFaceEmbeddings(
+    #     model_name="sentence-transformers/all-MiniLM-L6-v2",
+    #     model_kwargs={"device": "cpu"},
+    # )
+    df = pd.read_csv(data_path, sep="\t")
+    relevant_content = df["url"].values
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=CHUNK_SIZE,
+        chunk_overlap=CHUNK_OVERLAP,
+    )
+
     if not os.path.exists(DB_FAISS_PATH):
-        # docs = []
-        # metadata = []
-
-        # # Read PDF documents from the given path
-        # pdf_docs = [os.path.join(data_path, f) for f in os.listdir(data_path) if f.endswith('.pdf')]
-        # for pdf_path in pdf_docs:
-        #     with open(pdf_path, "rb") as pdf_file:
-        #         pdf_reader = PyPDF2.PdfReader(pdf_file)
-        #         for index, page in enumerate(pdf_reader.pages):
-        #             doc_page = {
-        #                 "title": os.path.basename(pdf_path) + " page " + str(index + 1),
-        #                 "content": page.extract_text(),
-        #             }
-        #             docs.append(doc_page)
-
-        # content = [doc["content"] for doc in docs]
-        # metadata = [{"title": doc["title"]} for doc in docs]
-        # print("Content and metadata are extracted from the documents")
-
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=CHUNK_SIZE,
-            chunk_overlap=CHUNK_OVERLAP,
-        )
         split_docs = text_splitter.create_documents(
-            df['url_content'].tolist(), 
-            metadatas=[{"title": row["url_title"], "url": row["url"]} for _, row in df.iterrows()]
+            df["url_content"].tolist(),
+            metadatas=[
+                {"title": row["url_title"], "url": row["url"]}
+                for _, row in df.iterrows()
+            ],
         )
         print(f"Documents are split into {len(split_docs)} passages")
 
@@ -66,48 +63,114 @@ def setupDbAndChain(data_path):
         db.save_local(DB_FAISS_PATH)
     else:
         print(f"Db already exists")
-        db = FAISS.load_local(DB_FAISS_PATH, embeddings, allow_dangerous_deserialization=True)
-    
+        db = FAISS.load_local(
+            DB_FAISS_PATH, embeddings, allow_dangerous_deserialization=True
+        )
+
+    # llm = llamacpp.LlamaCpp(
+    #     model_path=llama_model_path,
+    #     temperature=0.75,
+    #     max_tokens=200,
+    #     top_p=1,
+    #     n_ctx=3000,
+    #     verbose=False,
+    # )
     history_aware_retriever = create_history_aware_retriever(
-        ChatOpenAI(model=model_name, temperature=0), db.as_retriever(), ChatPromptTemplate.from_messages(
-        [
-            ("system", """Given a chat history and the latest user question, which may reference context from the chat history, you must formulate a standalone question that can be understood without the chat history. You are strictly forbidden from using any outside knowledge. Do not, under any circumstances, answer the question. Reformulate it if necessary; otherwise, return it as is."""),
-            MessagesPlaceholder("chat_history"),
-            ("human", "{input}"),
-        ]
-    ))
-    question_answer_chain = create_stuff_documents_chain(ChatOpenAI(model=model_name, temperature=0), ChatPromptTemplate.from_messages(
-        [
-            ("system", """As an assistant for question-answering tasks, your approach must be systematic and meticulous. First, identify CLUES such as keywords, phrases, contextual information, semantic relations, tones, and references that aid in determining the context of the input. Second, construct a concise diagnostic REASONING process (limiting to 130 words) based on premises supporting the INPUT relevance within the provided context. Third, utilizing the identified clues, reasoning, and input, furnish the pertinent answer for the question. Remember, you are required to use ONLY the provided context to answer the questions. If the question does not align with the context or if the context is absent, indicate that you don't know the answer. External knowledge is strictly prohibited. Failure to adhere will lead to incorrect answers. The context is as follows: {context}."""),
-            MessagesPlaceholder("chat_history"),
-            ("human", "{input}"),
-        ]
-    ))
-    rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
-    return rag_chain, relevant_content
+        # llm,
+        ChatOpenAI(model=model_name, temperature=0),
+        db.as_retriever(),
+        ChatPromptTemplate.from_messages(
+            [
+                (
+                    "system",
+                    """Given a chat history and the latest user question, which may reference context from the chat history, you must formulate a standalone question that can be understood without the chat history. You are strictly forbidden from using any outside knowledge. Do not, under any circumstances, answer the question. Reformulate it if necessary; otherwise, return it as is.""",
+                ),
+                MessagesPlaceholder("chat_history"),
+                ("human", "{input}"),
+            ]
+        ),
+    )
+    return history_aware_retriever, relevant_content
+
+def getChatQAChain(prompt):
+    question_answer_chain = create_stuff_documents_chain(
+        ChatOpenAI(model=model_name, temperature=0),
+        # llm,
+        ChatPromptTemplate.from_messages(
+            [
+                (
+                    "system",
+                    prompt + """ {context}.""",
+                ),
+                MessagesPlaceholder("chat_history"),
+                ("human", "{input}"),
+            ]
+        ),
+    )
+    return question_answer_chain
 
 
 load_dotenv(override=True)
-# chain = setupDbAndChain('../../documents/')
-chain, relevant_content = setupDbAndChain('../../131_webmd_vogon_sample1000_urlsContent_cleaned.tsv')
+# chain = setupDbAndChainRetreiver('../../documents/')
+history_aware_retriever, relevant_content = setupDbAndChainRetreiver(data_file_path)
+question_answer_chain = getChatQAChain("As an assistant for question-answering tasks, your approach must be systematic and meticulous. First, identify CLUES such as keywords, phrases, contextual information, semantic relations, tones, and references that aid in determining the context of the input. Second, construct a concise diagnostic REASONING process (limiting to 130 words) based on premises supporting the INPUT relevance within the provided context. Third, utilizing the identified clues, reasoning, and input, furnish the pertinent answer for the question. Remember, you are required to use ONLY the provided context to answer the questions. If the question does not align with the context or if the context is absent, indicate that you don't know the answer. External knowledge is strictly prohibited. Failure to adhere will lead to incorrect answers. The context is as follows:")
+rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
 chat_history = []
 curr_question_no = 1
-def echo(question, history):
-    global curr_question_no, chat_history
-    response = chain.invoke({"input": question, "chat_history": chat_history})
-    if len(response['context']) == 0 or "don't know" in response['answer'] or "cannot provide an answer" in response['answer'] or "I'm sorry" in response['answer']:
-        print(f"[ INVALID QUESTION ] {question} ----------> ", response['answer'])
+
+
+def chatWithRag(prompt, question):
+    global curr_question_no, chat_history, history_aware_retriever, question_answer_chain
+
+    if prompt != None or len(prompt):
+        question_answer_chain = getChatQAChain(prompt)
+        rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+    response = rag_chain.invoke({"input": question, "chat_history": chat_history})
+    if (
+        len(response["context"]) == 0
+        or "don't know" in response["answer"]
+        or "cannot provide an answer" in response["answer"]
+        or "I'm sorry" in response["answer"]
+    ):
+        print(f"[ INVALID QUESTION ] {question} ----------> ", response["answer"])
         return f"Answer: Question isn't relevant to provided context."
-    print(f"[ {curr_question_no} ] {question} ----------> ", response['answer'])
+    print(f"[ {curr_question_no} ] {question} ----------> ", response["answer"])
     curr_question_no += 1
     chat_history.extend([HumanMessage(content=question), response["answer"]])
     # if len(chat_history) > 3:
     #     chat_history = chat_history[1:]
     # print(chat_history)
-    docs_info = "\n\n".join([f"Title: {doc.metadata['title']}\nUrl: {doc.metadata['url']}\nContent: {doc.page_content}" for doc in response["context"]])
+    docs_info = "\n\n".join(
+        [
+            f"Title: {doc.metadata['title']}\nUrl: {doc.metadata['url']}\nContent: {doc.page_content}"
+            for doc in response["context"]
+        ]
+    )
     full_response = f"Answer: {response['answer']}\n\nRetrieved Documents:\n{docs_info}"
     return full_response
 
-demo = gr.ChatInterface(fn=echo, examples=list(relevant_content), title="RAG on webmd",theme=gr.themes.Soft(), fill_height=True)
+
+# demo = gr.ChatInterface(
+#     fn=echo,
+#     examples=list(relevant_content),
+#     title="RAG on webmd",
+#     inputs=[
+#         gr.inputs.Textbox(lines=1, placeholder="Enter the system prompt"),
+#         gr.inputs.Textbox(lines=1, placeholder="Enter the question")
+#     ],
+#     theme=gr.themes.Soft(),
+#     fill_height=True,
+# )
+
+with gr.Blocks() as demo:
+    gr.Markdown("# RAG on webmd")
+    with gr.Row():
+        prompt = gr.Textbox(lines=1, placeholder="Enter the system prompt.", label="System prompt")
+        question = gr.Textbox(lines=1, placeholder="Enter the question asked", label="Question")
+    output = gr.Textbox(label="Output")
+    submit_btn = gr.Button("Submit")
+    submit_btn.click(chatWithRag, inputs=[prompt, question], outputs=output)
+
 gr.close_all()
 demo.launch(share=True)
